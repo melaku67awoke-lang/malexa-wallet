be87314500b3a95dfa5488e674e15fb2f9cf7bf2
melaku67awoke-lang <melaku67awoke@gmail.com>
2026-09-25 02:17:51 +0300

    Refactor main.tsx to use App component
    Removed the App function definition and imported App component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <main>
-      <h1>Malexa Wallet</h1>
-      <p>Webapp is starting.</p>
-    </main>
-  );
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
